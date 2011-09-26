b696420d30c3fce0688c5f2b5a163ebb6a8665ae
djk <djk@isFiaD04zgAgnrEC5XJt1i4IE7AkNPqhBG5bONi6Yks>
2011-09-25 21:21:03 -0400

    Update doc.

--- release/cut_release.py
+++ release/cut_release.py
@@ -70,7 +70,7 @@ PUBLIC_SITE = "USK@kRM~jJVREwnN2qnA8R0Vt8HmpfRzBZ0j4rHC2cQ-0hw," + \
 
 FREENET_DOC_WIKI_IDX = 61
 FNIKI_IDX = 85
-REPO_IDX = 22
+REPO_IDX = 25
 
 ############################################################
 
